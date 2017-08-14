19d9fcfae39d51028a407994784ec1cca92ca8de
okako <kasanesn@hotmail.com>
2017-08-14 12:22:10 +0000

    responsive

--- src/app/layout/progressionby/progressionby.component.ts
+++ src/app/layout/progressionby/progressionby.component.ts
@@ -12,22 +12,12 @@ export class ProgressionbyComponent implements OnInit  {
 
   	@Input() max: number;
   	@Input() dynamic: number;
-  	@Input() type: string;
+  	@Input() notes: number;
  
   	public constructor() {}
 
   	ngOnInit() {
-  	 	let type;
-	    if (this.dynamic < 25) {
-	      type = 'success';
-	    } else if (this.dynamic < 50) {
-	      type = 'info';
-	    } else if (this.dynamic < 75) {
-	      type = 'warning';
-	    } else {
-	      type = 'danger';
-	    }
-	    this.type = type;
+  		this.notes = (this.dynamic * 100) / this.max;
   	}
 
 }
